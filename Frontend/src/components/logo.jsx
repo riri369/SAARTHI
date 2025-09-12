39f3226a08b4a759ef5c456dc572f579e2294381
Anjana Mahanta <147564598+Kasanjx@users.noreply.github.com>
2025-09-12 20:49:58 +0530

    Add files via upload

---
+++ Frontend/src/components/logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Styles/logo.css";
+
+export default function SaarthiLogo() {
+  return (
+    <div className="logo-container">
+      <span className="logo-icon">🛣️</span>
+      <span className="logo-text">Saarthi</span>
+    </div>
+  );
+}
